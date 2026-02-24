e09beb5f1b4074fe59702838ee52829d022b4864
collgreen <248493848@qq.com>
2026-02-25 00:06:27 +0800

    chore: 版本升级至 v1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.fangbianjizhang"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.2"
+        versionCode = 6
+        versionName = "1.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
